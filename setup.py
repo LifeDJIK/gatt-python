5f1cb3f4a54ea0b7ec6b2532b41ffa20497427a2
Ivan Krakhmaliuk <lifedj27@gmail.com>
2025-04-28 20:10:00 +0300

    Version update for local pip installs

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='gatt',
     packages=['gatt'],
-    version='0.2.7',
+    version='0.2.7.post1',
     description='Bluetooth GATT SDK for Python',
     keywords='gatt',
     url='https://github.com/getsenic/gatt-python',
